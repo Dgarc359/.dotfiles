0c264ae079397c029ccb027c79666d0e718a4eec
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-07-25 04:11:25 -0400

    nvim with lsp

--- nvim/.config/nvim/lua/dgarc/plugins.lua
+++ nvim/.config/nvim/lua/dgarc/plugins.lua
@@ -3,12 +3,14 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
+	-- fuzzy find files
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
+	-- color scheme
 	use({
 		'rose-pine/neovim',
 		as = 'rose-pine',
@@ -16,4 +18,31 @@ return require('packer').startup(function(use)
 			vim.cmd('colorscheme rose-pine')
 		end
 	})
+
+	use({'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }})
+	use('nvim-treesitter/playground')
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+
+	use {
+  		'VonHeikemen/lsp-zero.nvim',
+  		branch = 'v2.x',
+  		requires = {
+    		-- LSP Support
+    			{'neovim/nvim-lspconfig'},             -- Required
+    			{                                      -- Optional
+      				'williamboman/mason.nvim',
+      				run = function()
+        			  pcall(vim.api.nvim_command, 'MasonUpdate')
+      				end,
+    			},
+    		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    		-- Autocompletion
+    		{'hrsh7th/nvim-cmp'},     -- Required
+    		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+    		{'L3MON4D3/LuaSnip'},     -- Required
+  	}
+}
 end)
--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -74,10 +74,60 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  LuaSnip = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  ["cmp-nvim-lsp"] = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
+  harpoon = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/harpoon",
+    url = "https://github.com/theprimeagen/harpoon"
+  },
+  ["lsp-zero.nvim"] = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    url = "https://github.com/VonHeikemen/lsp-zero.nvim"
+  },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
+  },
+  ["nvim-cmp"] = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    url = "https://github.com/hrsh7th/nvim-cmp"
+  },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/Users/vader/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
+  },
+  playground = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/playground",
+    url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
@@ -94,6 +144,16 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/vader/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  undotree = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/undotree",
+    url = "https://github.com/mbbill/undotree"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/Users/vader/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
   }
 }
 
