d9b803c02c27b9647f258bbdc6b1621fe739ca35
dgarcia <dgarcia@collegeboard.org>
2024-04-01 15:02:30 -0400

    quick buffer switch with telescope

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,16 @@
 local builtin = require('telescope.builtin')
+
+require('telescope').setup {
+    pickers = {
+        buffers = {
+            sort_mru = true,
+        },
+    },
+}
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope buffers<CR>", {})
+vim.keymap.set('n', '<leader><C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
 end)
