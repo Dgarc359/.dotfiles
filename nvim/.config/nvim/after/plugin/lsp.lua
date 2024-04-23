b2163be201803f8cebc58d966fa2972497028be8
dgarcia <dgarcia@collegeboard.org>
2024-04-23 17:10:50 -0400

    add super tab config and snippet config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -66,15 +66,79 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
-require'cmp'.setup {
+local luasnip = require("luasnip")
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local t = ls.text_node
+
+luasnip.add_snippets("all", {
+	s("autotrigger", {
+		t("autosnippet"),
+	}),
+}, {
+	type = "autosnippets",
+	key = "all_auto",
+})
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup {
+snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
   sources = {
-    { name = 'nvim_lsp' }
-  }
+    { name = 'nvim_lsp' },
+        { name = 'luasnip', keyword_length = 2 },
+  },
+    window = {
+        documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+    },
+    mapping = {
+        ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                cmp.confirm({
+                    select = true,
+                })
+            end
+        else
+            fallback()
+        end
+    end),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	    ['<C-y>'] = cmp.mapping.confirm({select = true}),
+	    ["<C-Space>"] = cmp.mapping.complete(),
+    }
 }
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
 
 --[[ lsp.preset('recommended')
 
@@ -84,43 +148,7 @@ lsp.set_preferences({
 })
 
 
-lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_pref() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
-local cmp_action = require('lsp-zero').cmp_action()
-
-lsp.setup()
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
 local cmp_config = lsp.defaults.cmp_config({
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    window = {
-        documentation = cmp.config.window.bordered(),
-        completion = cmp.config.window.bordered(),
-    },
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip', keyword_length = 2 },
-    }),
-    mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 	    ['<C-y>'] = cmp.mapping.confirm({select = true}),
 	    ["<C-Space>"] = cmp.mapping.complete(),
