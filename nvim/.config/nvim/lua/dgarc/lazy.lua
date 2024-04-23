875d932817faad4aa2904431ba101fc7ddfaa476
dgarcia <dgarcia@collegeboard.org>
2024-04-23 15:38:33 -0400

    floating window config for nvim-tree

--- nvim/.config/nvim/lua/dgarc/lazy.lua
+++ nvim/.config/nvim/lua/dgarc/lazy.lua
@@ -30,8 +30,36 @@ require('lazy').setup({
         config = function()
             -- config https://github.com/MarioCarrion/videos/blob/269956e913b76e6bb4ed790e4b5d25255cb1db4f/2023/01/nvim/lua/plugins/nvim-tree.lua
             require("nvim-tree").setup {
+                disable_netrw = true,
+                hijack_netrw = true,
+                respect_buf_cwd = true,
+                sync_root_with_cwd = true,
                 view = {
-                    width = 30,
+                    width = function()
+                        return math.floor(vim.opt.columns:get() * 0.5)
+                    end,
+                    float = {
+                        enable = true,
+                        open_win_config = function()
+                            local screen_w = vim.opt.columns:get()
+                            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                            local window_w = screen_w * 0.5
+                            local window_h = screen_h * 0.8
+                            local window_w_int = math.floor(window_w)
+                            local window_h_int = math.floor(window_h)
+                            local center_x = (screen_w - window_w) / 2
+                            local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                                             - vim.opt.cmdheight:get()
+                            return {
+                              border = "rounded",
+                              relative = "editor",
+                              row = center_y,
+                              col = center_x,
+                              width = window_w_int,
+                              height = window_h_int,
+                            }
+                        end,
+                    },
                 },
                 actions = {
                     open_file = {
@@ -96,10 +124,23 @@ require('lazy').setup({
     'hrsh7th/cmp-nvim-lsp',
     -- dap
 
-    "mfussenegger/nvim-dap",
+            "mfussenegger/nvim-dap",
+    --{
+	--"rcarriga/nvim-dap-ui",
+    --    dependencies = {
+    --        "mfussenegger/nvim-dap"
+    --    },
+    --    config = function()
+    --        require('dapui').setup()
+    --    end,
+    --},
 
     "theHamsta/nvim-dap-virtual-text",
 
+    -- comments
+    {
+        "kkoomen/vim-doge",
+    },
     --
     'christoomey/vim-tmux-navigator',
 
