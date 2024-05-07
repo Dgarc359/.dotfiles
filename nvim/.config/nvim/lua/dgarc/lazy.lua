6b0d0cd15e80c75a00347405d1e528b39ab072df
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2024-05-07 14:40:07 -0400

    add status column and lua formatting

--- nvim/.config/nvim/lua/dgarc/lazy.lua
+++ nvim/.config/nvim/lua/dgarc/lazy.lua
@@ -14,7 +14,23 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
 
     -- text folding
-    { 'kevinhwang91/nvim-ufo', dependencies = { 'kevinhwang91/promise-async'}},
+    { 'kevinhwang91/nvim-ufo', dependencies = {
+        'kevinhwang91/promise-async',
+        {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
+    }},
 
 	-- fuzzy find files
 	{
